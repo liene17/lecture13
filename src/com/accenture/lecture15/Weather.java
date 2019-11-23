acbb46a1b0d8f8cbb362e5095c445e7732fa7214
Liene <rudukaliene@gmail.com>
2019-11-23 11:55:26 +0200

    task2

--- src/com/accenture/lecture15/Weather.java
+++ src/com/accenture/lecture15/Weather.java
@@ -1,12 +1,14 @@
 package com.accenture.lecture15;
 
 public class Weather {
-    String country;  //for which forecast is prepared.
-	int[] daysWeather;  //temperature information for next week (7 days)
+	String country; // for which forecast is prepared.
+	int[] daysWeather; // temperature information for next week (7 days)
 
-	
+	public Weather(String country, int[] daysWeater) {
+
+	}
+
 	public static void main(String[] args) {
-		
 
 	}
 
