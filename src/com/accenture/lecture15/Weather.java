687ddf1f43fe0fa7d7cdb223b9bc92c77934abb8
Liene <rudukaliene@gmail.com>
2019-11-23 11:53:16 +0200

    task1

--- src/com/accenture/lecture15/Weather.java
+++ src/com/accenture/lecture15/Weather.java
@@ -1,12 +1,12 @@
 package com.accenture.lecture15;
 
 public class Weather {
+    String country;  //for which forecast is prepared.
+	int[] daysWeather;  //temperature information for next week (7 days)
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
